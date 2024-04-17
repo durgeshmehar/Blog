547cdc88678c8d7d09553f204eebd7c3cfdba6c1
Durgesh Mehar <durgeshmehar2002@gmail.com>
2024-04-18 00:07:46 +0530

    render deployment 1

--- mysite/build_files.sh
+++
@@ -1,5 +0,0 @@
-echo "Building files..."
-python3.9 -m venv venv && source venv/bin/activate
-python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput --clear
-echo "Building end."
