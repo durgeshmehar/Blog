f0772358983bca75d7125e20a2d88149701ca751
Durgesh Mehar <durgeshmehar2002@gmail.com>
2024-04-17 17:02:20 +0530

    gemini suggestion for deployment

--- mysite/build_files.sh
+++ mysite/build_files.sh
@@ -1,2 +1,3 @@
-python -m venv venv && source venv/bin/activate && pip install -r requirements.txt
+source venv/bin/activate  # Activate the virtual environment
+venv/bin/pip install -r requirements.txt  # Use virtual environment's pip
 python3.9 manage.py collectstatic
